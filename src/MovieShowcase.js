27c2340fe503398282ddfb5da408385b482d2c8f
github username <github email address>
2020-08-05 15:02:53 -0400

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -5,10 +5,21 @@ import movieData from './data.js'
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
-    // map over your movieData array and return an array of the correct JSX
-  }
+    console.log(this.props)
+
+    return movieData.map((movie, index) => <MovieCard
+      key={index}
+      title = {movie.title}
+      IMDBRating = {movie.IMDBRating}
+      genres = {movie.genres}
+      poster = {movie.poster}
+
+    />)
+
+  };
 
   render() {
+
     return (
       <div id="movie-showcase">
         {this.generateMovieCards()}
@@ -16,3 +27,5 @@ export default class MovieShowcase extends Component {
     )
   }
 }
+//If we were to place console.log(this.props) in the MovieCard component at the beginning
+//of render(), we'd see that each MovieCard contains different props.
--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -12,16 +12,17 @@ export default class CardBack extends Component {
 
   generateRatingElement = () => {
     // implement meeeee! See the readme for instructions
+    //genre, titles, imbd
   }
 
   render() {
     return (
       <div className="card-back">
-        <h3 className="title"></h3>
+        <h3 className="title">{this.props.title}</h3>
         <span />
         { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
         <span />
-        <h5 className="genres"></h5>
+        <h5 className="genres">{this.props.genres.join(', ')}</h5>
       </div>
     )
   }
